416b6f76b2c8d5357f4d82ad2d422ed6b35af8d0
Katrine-Marie Burmeister <fjordscene@gmail.com>
2019-11-01 01:05:03 +0100

    Add ASCII chars to string

--- classes/PasswordGenerator.php
+++ classes/PasswordGenerator.php
@@ -7,6 +7,23 @@
     }
     
     private function generatePasswordString(){
+      // Generer tekststreng
+      // ASCII tal
+      for($i = 48; $i <= 57; $i++){
+        $allChars .= chr($i);
+      }
+      // ASCII store bogstaver
+      for($i = 65; $i <= 90; $i++){
+        $allChars .= chr($i);
+      }
+      // ASCII små bogstaver
+      for($i = 97; $i <= 122; $i++){
+        $allChars .= chr($i);
+      }
+
+      // Tilføj udvalgte specialtegn til tekststreng
+      $allChars .= ".,!?#()[]=%&~^:;-_";
+      
     }
     
     public function returnNewPassword(){
